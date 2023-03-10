49d686e4f2b78b9b09b8623dd34572ed25d061d4
davyn <davyn@uci.edu>
2023-03-09 23:49:26 -0800

    Add keys.py to hide API key. Adjusted init.py to hide API key. Add database and keys to gitignore for security.

--- Website/__init__.py
+++ Website/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+import keys
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -9,7 +10,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'No One Would Guess This'
+    app.config['SECRET_KEY'] = keys.Secret_Key
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
